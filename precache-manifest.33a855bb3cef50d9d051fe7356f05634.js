9952bf0bb1bce49ade102490ec68ceaad36f2222
bvl <bennyspammy@gmail.com>
2020-03-01 22:07:00 +0100

    build init

---
+++ precache-manifest.33a855bb3cef50d9d051fe7356f05634.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "158c04861112ffb032b47cebe8760148",
+    "url": "/index.html"
+  },
+  {
+    "revision": "433edca853fb81d490c0",
+    "url": "/static/css/main.e01a5209.chunk.css"
+  },
+  {
+    "revision": "8ef458cd99832ff1792d",
+    "url": "/static/js/2.c0ff3bef.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.c0ff3bef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "433edca853fb81d490c0",
+    "url": "/static/js/main.8867f4af.chunk.js"
+  },
+  {
+    "revision": "d7f8999897576f3b9171",
+    "url": "/static/js/runtime-main.1e5945d9.js"
+  }
+]);
